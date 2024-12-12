142bd9a8a3506480daf7a148511d896e938c1fd3
Nozferatu <sparrowhawk2005@gmail.com>
2024-12-12 15:53:14 +0100

    Commit inicial

---
+++ app/build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.cmj.firebasecrud_guitarras"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.cmj.firebasecrud_guitarras"
+        minSdk = 30
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    //Firebase
+    implementation ("com.google.firebase:firebase-database:20.0.2")
+    implementation ("com.google.firebase:firebase-core:20.0.0")
+    implementation ("com.google.firebase:firebase-storage:20.0.0")
+    implementation ("com.google.firebase:firebase-auth:21.0.1")
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    implementation ("com.google.firebase:firebase-analytics:20.0.2")
+    implementation ("com.google.android.gms:play-services-auth:20.1.0")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.5.1")
+
+    //AppWriteStorage
+    implementation("io.appwrite:sdk-for-kotlin:5.0.1")
+}
